0cdd542d244a42acd9a06af887304fbc084838ed
Spencer Takata <spencer.takata@gmail.com>
2024-01-05 16:48:47 -0800

    Finished Gatsby tutorial part 5

---
+++ gatsby_tutorial/gatsby-config.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+    title: "My Gatsby Tutorial",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        classPrefix: "language-",
+      },
+    },
+  ],
+};
